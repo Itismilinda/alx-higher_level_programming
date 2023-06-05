5808617cac89f0bc5e3c2b11fe4c1f0735d393d7
Patience <patience.daniel.kindai@gmail.com>
2023-06-05 11:56:30 -0700

    updated

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-
 word = "Holberton"
-# Extract the first three letters from the word
-first_three_letters = word[:3]
-
-# Extract the last two letters from the word
-last_two_letters = word[-2:]
-
-# Extract the middle portion of the word (excluding the first and last letters)
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
 middle_word = word[1:-1]
-
-# Print the results
-print("First 3 letters: {}".format(first_three_letters))
-print("Last 2 letters: {}".format(last_two_letters))
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
+
