6f2e3b1ec104bcf9c0bd756d31358800a1d2ae56
Patience <patience.daniel.kindai@gmail.com>
2023-06-05 12:03:09 -0700

    updated

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdlib.h>
 
 /**
@@ -13,8 +13,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
