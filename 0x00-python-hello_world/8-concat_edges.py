cef50a682cb0744f56d249058435d4e32c4bc6a5
Patience <patience.daniel.kindai@gmail.com>
2023-06-05 11:58:50 -0700

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-text = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-substring = text[39:67] + text[107:112] + text[:6]
-print(substring)
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
