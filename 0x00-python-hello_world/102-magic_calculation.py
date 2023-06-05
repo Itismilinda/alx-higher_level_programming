614ca34615955a41f79d08f5561c068ff1ddba09
Patience <patience.daniel.kindai@gmail.com>
2023-06-05 12:06:11 -0700

    Updated

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
-    result = 0
-    if a == 1 and b == 2:
-        result = 3
-    elif a == 3:
-        result = b ** 2 + 98
-    else:
-        result = a * b - 98
-    return result
+    return (98 + a ** b)
+
